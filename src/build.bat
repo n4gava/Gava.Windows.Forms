c68ef48dc5521a0fd39f47ce846108f6afa20dc0
Guilherme Gavazzoni <gg_numbuh4@hotmail.com>
2017-12-29 15:56:49 -0200

    Alteração no build

--- src/build.bat
+++ src/build.bat
@@ -1,1 +1,17 @@
+@echo Off
+ 
+set version=1.0.0
+if not "%PackageVersion%" == "" (
+   set version=%PackageVersion%
+)
+
+set nuget=
+if "%nuget%" == "" (
+	set nuget=nuget
+)
+
 "%MsBuildExe%" src\Gava.Windows.Forms.sln /p:Configuration=Release /p:Platform="Any CPU"
+
+mkdir Build
+mkdir Build\lib
+mkdir Build\lib\net40
